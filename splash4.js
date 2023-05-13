592f68fa9d9d67e293ca0f750569c33d33a77f34
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2023-05-13 20:56:28 +0800

    Add files via upload

---
+++ splash4.js
@@ -0,0 +1,10 @@
+splashList = [
+'Jangan takut terhadap orang di posisi manapun, rendah ataupun tinggi, entah dia cendekiawan atau orang yang tidak berpengetahuan. Kalau kau hormat kepada semua manusia, kau harus mengasihi semua orang, dan tidak merasa takut kepada siapapun.',
+'Jangan biarkan jati diri menyatu dengan pekerjaan Anda. Jika pekerjaan lenyap, maka jati diri Anda tidak akan pernah hilang.',
+'Surga dan Bumi itu abadi. Keduanya abadi karena tidak ada bagi dirinya sendiri. Dengan cara yang sama, orang yang benar-benar suci tak hanya hidup bagi dirinya sendiri, karenanya bisa menjadi abadi dan meraih apapun.',
+'Manangguak di aia karuah.',
+'Menangguk di air keruh.',
+'Mencari keuntungan dalam kekacauan.',
+'Tiga hal siapa yang melakukannya, maka ia berpotensi mendapatkan murka: tertawa tanpa ada sesuatu yang mengejutkan, tidur tanpa begadang, dan makan tanpa rasa lapar.',
+'Jika Amerika Serikat salah, harapan apa yang kita miliki dari dunia beradab pada gilirannya?',
+]
