7fa330bd29090bb31715da534d7dec854f41fa2c
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2023-05-13 10:53:50 +0800

    Add files via upload

---
+++ splash3.js
@@ -0,0 +1,11 @@
+splashList = [
+'Perjuangan bangsa Indonesia sebelum abad ke-20 bersifat lokal atau kedaerahan.',
+'Perjuangan bangsa Indonesia sebelum abad ke-20 dilakukan secara fisik dengan senjata tradisional.',
+'Perjuangan bangsa Indonesia sebelum abad ke-20 dipimpin tokoh yang karismatik.',
+'Perjuangan bangsa Indonesia sebelum abad ke-20 bersifat sporadis.',
+'Bipatride merupakan kondisi saat seorang penduduk mempunyai dua macam kewarganegaraan sekaligus atau dikenal sebagai kewarganegaraan ganda.',
+'Bipatride dapat terjadi saat seorang anak keturunan bangsa B yang menganut asas <i>ius sanguinis</i> lahir di negara A yang menganut asas <i>ius soli</i>.',
+'Dokuritsu Junbi Cosakai atau BPUPKI didirikan oleh Kumaikici Harada pada tanggal 1 Maret 1945 dengan maksud agar Indonesia membantu Jepang melawan sekutu.',
+'Sistem pertahanan dan keamanan Indonesia menurut UUD 1945 adalah upaya pertahanan dan keamanan negara yang dilaksanakan melalui sistem pertahanan dan keamanan rakyat semesta.',
+
+]
