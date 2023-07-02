ab6f5e4f421010c8e878d070179dd58d0942939b
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2023-07-02 08:27:42 +0800

    Add files via upload

---
+++ splash6.js
@@ -0,0 +1,10 @@
+splashList = [
+'Jangan pakai retinol di wajah yang basah',
+'Pastikan wajah sudah benar-benar kering saat memakai retinol',
+'Retinol di wajah yang basah dapat mengakibatkan iritasi',
+'Jangan terburu-buru menaikkan frekuensi penggunaan retinol',
+'Jangan mulai penggunaan retinol dengan dosis tinggi, mulailah dari dosis 0,5% untuk pemula',
+'Jangan campurkan retinol yang akan digunakan di wajah dengan vitamin C',
+'Jangan campurkan retinol yang akan digunakan di wajah dengan AHA',
+'Jangan campurkan retinol yang akan digunakan di wajah dengan BHA',
+]
